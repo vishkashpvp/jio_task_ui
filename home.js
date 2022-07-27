8048699f4118033a2cd32dc7fe9406d918cb1e17
Vishkash <vishkash.vishnu@gmail.com>
2022-07-27 14:05:45 +0530

    Display users array in table

--- home.js
+++ home.js
@@ -9,6 +9,67 @@ currentUserEmployees = getCurrentUserEmployees();
 document.title = "Home - " + currentUser.name;
 document.querySelector("#name_span").textContent = currentUser.name;
 document.querySelector("#mail_span").textContent = currentUser.mail;
+let employees_table = document.getElementById("employees_table");
+
+setEmployeesDataTable();
+
+/**
+ * sets current user employees data to table in html
+ */
+function setEmployeesDataTable() {
+  employees_table.width = "100%";
+  employees_table.border = "2px solid black";
+
+  currentUserEmployees.forEach((employee) => {
+    let tr = employees_table.insertRow();
+    let name = tr.insertCell();
+    let mail = tr.insertCell();
+    let mobile = tr.insertCell();
+    let tech = tr.insertCell();
+    let action = tr.insertCell();
+
+    name.align = "center";
+    mail.align = "center";
+    mobile.align = "center";
+    tech.align = "center";
+    action.align = "center";
+
+    edit_button = document.createElement("button");
+    edit_button.innerHTML = "edit";
+    edit_button.onclick = function () {
+      editEmployee(employee);
+    };
+    delete_button = document.createElement("button");
+    delete_button.innerHTML = "delete";
+    delete_button.onclick = function () {
+      deleteEmployee(employee);
+    };
+
+    name.appendChild(document.createTextNode(employee.name));
+    mail.appendChild(document.createTextNode(employee.mail));
+    mobile.appendChild(document.createTextNode(employee.mobile));
+    tech.appendChild(document.createTextNode(employee.tech));
+    action.appendChild(edit_button);
+    action.appendChild(delete_button);
+  });
+}
+
+/**
+ * edit employee details
+ * @param employee
+ */
+function editEmployee(employee) {
+  console.log(employee);
+}
+
+/**
+ * delete employee from employees array
+ * @param employee
+ */
+function deleteEmployee(employee) {
+  console.log("deletingg...");
+  console.log(employee);
+}
 
 /**
  * @returns current user object from local storage
