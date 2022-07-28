ca9851a07d7946c81cbb1679dea40d13644d06b7
Vishkash <vishkash.vishnu@gmail.com>
2022-07-28 15:38:36 +0530

    Migrate validation to utils folder

--- add-employee.js
+++ add-employee.js
@@ -68,9 +68,7 @@ getCurrentUserEmployees = () => {
 };
 
 /**
- * adds employee to employees array of current user
- * and employees array of all users
- * in local storage
+ * adds employee to employees array in local storage
  * @param employee
  */
 setEmployee = (employee) => {
@@ -90,69 +88,6 @@ validEmployeeDetails = () => {
     validEmployeeMail(employee_mail.value) &&
     validEmployeeMobile(employee_mobile.value)
   );
-};
-
-/**
- * @param employeeName
- * @returns boolean indicating provided employeeName fulfills conditions
- */
-validEmployeeName = (employeeName) => {
-  if (isEmpty(employeeName)) {
-    window.alert("Employee name cannot be empty");
-    return false;
-  }
-  if (employeeName.length < 4) {
-    window.alert("Employee name should be atleast 4 characters long");
-    return false;
-  }
-  return true;
-};
-
-/**
- * @param employeeMobile
- * @returns boolean indicating provided employeeMobile fulfills conditions
- */
-validEmployeeMobile = (employeeMobile) => {
-  if (isEmpty(employeeMobile)) {
-    window.alert("Employee mobile cannot be empty");
-    return false;
-  }
-  if (isNaN(parseInt(employeeMobile))) {
-    window.alert("Mobile number should contain only munerical values");
-    return false;
-  }
-  if (employeeMobile.length !== 10) {
-    window.alert("Employee mobile should be 10 characters");
-    return false;
-  }
-  return true;
-};
-
-/**
- * @param employeeMail
- * @returns boolean indicating provided employeeMail satisfies conditions
- */
-validEmployeeMail = (employeeMail) => {
-  if (isEmpty(employeeMail)) {
-    window.alert("Employee mail cannot be empty");
-    return false;
-  }
-  if (
-    !employeeMail.toLowerCase().endsWith("@ril.com") &&
-    !employeeMail.toLowerCase().endsWith("@gmail.com")
-  ) {
-    window.alert("Invalid mail format provided");
-    return false;
-  }
-  return true;
-};
-
-/**
- * @param value
- * @returns boolean indicating provided value is empty
- */
-isEmpty = (value) => {
-  return !value || value.length === 0;
 };
 
 /**
